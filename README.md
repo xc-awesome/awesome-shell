81186d253ad638f448e22e4f57e1c0e7964810fa
whj5002120 <whj5002120@ixiancheng.com>
2020-10-16 10:36:27 +0800

    fix

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Use of guidelines
 ├── jumpuser.sh       # jump user install
 └── root.sh           # root user install
 ```
+
+# jump user install command
+```shell script
+sh -c "$(curl -fsSL https://git.io/xc-install-root)"
+```
+
+# root user install command
+```shell script
+sh -c "$(curl -fsSL https://git.io/xc-install-jumpuser)"
+```
--- root.sh
+++ root.sh
@@ -4,31 +4,29 @@ echo  ::::::: Begin installing and configuring jumpuser users :::::::
 
 echo Change user to root
 
-sudo -i
-
 echo Yum install zsh ...
-yum install zsh -y
+sudo yum install zsh -y
 
 echo Yum install git
-yum install git -y
+sudo yum install git -y
 
 echo Yum install oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo Git clone oh my zsh plugin for zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 echo Git clone oh my zsh plugin for zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 echo Git clone oh my zsh plugin for autojump
-git clone git://github.com/wting/autojump.git  $ZSH_CUSTOM/plugins/autojump
+sudo git clone git://github.com/wting/autojump.git  $ZSH_CUSTOM/plugins/autojump
 
 echo Install autojump plugin
-cd $ZSH_CUSTOM/plugins/autojump; ./install.py
+sudo cd $ZSH_CUSTOM/plugins/autojump; ./install.py
 
 echo Configuration ohmyzsh environment
-wget https://raw.githubusercontent.com/xc-awesome/ohmyzsh-install/main/.zshrc-root -O ~/.zshrc
+sudo wget https://raw.githubusercontent.com/xc-awesome/ohmyzsh-install/main/.zshrc-root -O ~/.zshrc
 
 echo Source ~/.zshrc
-source ~/.zshrc
+sudo source ~/.zshrc
